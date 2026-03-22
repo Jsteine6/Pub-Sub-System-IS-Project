dd3005d8d3c6f46df81d3c637acbf20eba9638b2
Dan-oren16 <156258454+Dan-oren16@users.noreply.github.com>
2026-03-22 16:29:40 -0400

    Uploaded consumer component
    This consumer component does the following: 
    - Connects to Olivia's Google Pub/Sub subscription ('alert-messaging-topic-sub')
    - Creates a file for the sqlite3 database ('messaging-database') and creates a table
    - Receives, decodes, and parses messages from the subscription using a streaming pull method
    - Validates message structure and accurate data types
    - checks for duplicate 'message_id' against the database
    - Inserts message payload into the database and a duplicate flag ('is_duplicate') 
    - Acknowledges the message after saved
    - Has some error handling

---
+++ consumer.py
@@ -0,0 +1,105 @@
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
+import json
+import sqlite3
+
+#GCP variables
+project_id = "propane-bearing-485422-u7"
+subscription_id = "alert-messaging-topic-sub"
+timeout = 30.0 # Number of seconds the subscriber should listen for messages
+
+# sqlite3 Setup - creates DB file
+con = sqlite3.connect('messaging-database', check_same_thread=False)
+cursor = con.cursor() # cursor object to execute sql commands
+
+# create a database if it doesn't exist
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    message_id TEXT UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    created_at TEXT NOT NULL,
+    is_duplicate BOOLEAN
+)
+''')
+con.commit()
+
+# Establish connection to pubsub subscription
+# 'subscriber_path' creates a path that looks like this: projects/{project_id}/topics/{topic_id}
+subscriber = pubsub_v1.SubscriberClient()
+subscriber_path = subscriber.subscription_path(project_id, subscription_id)
+
+# Check if payload is missing any fields and accurate data types
+def validate_payload(payload: dict):
+    required_fields = ["message_id", "content", "created_at"]
+
+    for field in required_fields:
+        if field not in payload:
+            raise ValueError(f"Missing required field: {field}")
+
+    if not isinstance(payload["message_id"], str):
+        raise ValueError("message_id must be a string")
+
+    if not isinstance(payload["content"], str):
+        raise ValueError("content must be a string")
+
+    if not isinstance(payload["created_at"], str):
+        raise ValueError("created_at must be a string")
+
+# check for duplicates
+def check_duplicate(message_id: str) -> bool:
+    cursor.execute(
+        "SELECT EXISTS(SELECT 1 FROM messages WHERE message_id = ?)",
+        (message_id,)
+    )
+    return bool(cursor.fetchone()[0])
+
+# Insert message into DB
+def insert_message(payload: dict, is_duplicate: bool):
+    cursor.execute("""
+        INSERT INTO messages (message_id, content, created_at, is_duplicate)
+        VALUES (?, ?, ?, ?)
+    """, (
+        payload["message_id"],
+        payload["content"],
+        payload["created_at"],
+        is_duplicate
+    ))
+    con.commit()
+
+
+# receive message from pubsub subscription
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    try:
+        # decode and parse message
+        decoded = message.data.decode("utf-8")
+        payload = json.loads(decoded)
+        print("Received payload:", payload)
+
+        # validate payload structure
+        validate_payload(payload)
+        message_id = payload["message_id"]
+
+        # check for duplicates
+        is_duplicate = check_duplicate(message_id) 
+
+        # insert message to DB and add duplicate flag
+        insert_message(payload, is_duplicate)
+        print("Message added to database: ", payload, is_duplicate)
+
+        # ack message
+        message.ack()
+
+    except Exception as e:
+        print("Error processing message:", e)
+        # ack invalid messages to infinite loop
+        message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscriber_path, callback=callback)
+print(f"Listening for messages on {subscriber_path}\n")
+
+try:
+    streaming_pull_future.result()
+except KeyboardInterrupt:
+    streaming_pull_future.cancel()
+    streaming_pull_future.result()
